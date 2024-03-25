345e1499f2c438c438c1c94cdec9855a06c6b167
Robert Kubasiak <robert2006goetel@gmail.com>
2024-03-25 20:17:53 +0100

    mux init

---
+++ LiturgyLog/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+var db *sql.DB
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+
+	var err error
+	db, err = sql.Open("mysql", "root:secret@(localhost:3306)/db?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	r.HandleFunc("/", index)
+	http.ListenAndServe(":8080", r)
+}
