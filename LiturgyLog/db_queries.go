413c81b9a538d9ee00bd6cc62565e8e71bd068be
Robert Kubasiak <robert2006goetel@gmail.com>
2024-03-26 18:31:28 +0100

    getting data from queries by references

--- LiturgyLog/db_queries.go
+++ LiturgyLog/db_queries.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
 )
 
-func scanRankRows(target []person, rows *sql.Rows) {
+func scanRankRows(target *[]person, rows *sql.Rows) {
 	for rows.Next() {
 		var p person
 		err := rows.Scan(&p.FirstName, &p.LastName, &p.Card, &p.MPoints, &p.GPoints, &p.Sum)
 		if err != nil {
 			log.Fatal(err)
-
 		}
 
-		target = append(target, p)
+		*target = append(*target, p)
 	}
 }
 
-func scanActivityRows(target []activity, rows *sql.Rows) {
+func scanActivityRows(target *[]activity, rows *sql.Rows) {
 	for rows.Next() {
 		var a activity
 
@@ -33,59 +34,58 @@ func scanActivityRows(target []activity, rows *sql.Rows) {
 			log.Fatal(err)
 
 		}
-		target = append(target, a)
+		*target = append(*target, a)
 	}
 
 }
 
 func queryRank() ([]person, []person) {
-	q := "SELECT osoby.Imie, osoby.Nazwisko, osoby.karta, osoby.zbiorki, osoby.Punkty, osoby.suma FROM `osoby` WHERE stopien = '?' ORDER BY suma DESC"
-
-	rA, err := db.Query(q, "M")
+	baseQuery := "SELECT osoby.Imie, osoby.Nazwisko, osoby.karta, osoby.zbiorki, osoby.Punkty, osoby.suma FROM `osoby` WHERE stopien = %s ORDER BY suma DESC"
+	rA, err := db.Query(fmt.Sprintf(baseQuery, "\"M\""))
 	if err != nil {
 		log.Fatal(err)
 
 	}
 
-	rL, err := db.Query(q, "L")
+	rL, err := db.Query(fmt.Sprintf(baseQuery, "\"L\""))
 	if err != nil {
 		log.Fatal(err)
 
 	}
+
+	var l, a []person
+	scanRankRows(&l, rL)
+	scanRankRows(&a, rA)
 
 	defer rA.Close()
 	defer rL.Close()
-
-	var l, a []person
-	scanRankRows(l, rL)
-	scanRankRows(a, rA)
 
 	return l, a
 }
 
 func queryActivities(mode string) []activity {
-	q := "SELECT osoby.Imie, osoby.Nazwisko, odczyt.msza_godz, odczyt.data_odczytu FROM osoby, odczyt WHERE osoby.karta = odczyt.karta ? ORDER BY odczyt.data_odczytu, odczyt.msza_godz ?"
-
-	var err error
-	var rows *sql.Rows
-
-	defer rows.Close()
+	baseQuery := "SELECT osoby.Imie, osoby.Nazwisko, odczyt.msza_godz, odczyt.data_odczytu FROM osoby, odczyt WHERE osoby.karta = odczyt.karta"
+	var query string
 
 	switch mode {
 	case "last10":
-		rows, err = db.Query(q, "", "LIMIT 10")
+		query = fmt.Sprintf("%s ORDER BY odczyt.data_odczytu DESC, odczyt.msza_godz DESC LIMIT 10", baseQuery)
 	case "thisMonth":
-		rows, err = db.Query(q, "\"and odczyt.data_odczytu > CURRENT_DATE - INTERVAL 1 MONTH\"", "")
+		query = fmt.Sprintf("%s AND odczyt.data_odczytu > CURRENT_DATE - INTERVAL 1 MONTH ORDER BY odczyt.data_odczytu DESC, odczyt.msza_godz DESC", baseQuery)
 	case "thisWeek":
-		rows, err = db.Query(q, "\"and odczyt.data_odczytu > CURRENT_DATE - INTERVAL 1 WEEK\"", "")
+		query = fmt.Sprintf("%s AND odczyt.data_odczytu > CURRENT_DATE - INTERVAL 1 WEEK ORDER BY odczyt.data_odczytu DESC, odczyt.msza_godz DESC", baseQuery)
+	default:
+		log.Fatalln("Incorrect queryActivities() mode")
 	}
+
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
-
 	}
+	defer rows.Close()
 
 	var a []activity
-	scanActivityRows(a, rows)
+	scanActivityRows(&a, rows)
 
 	return a
 }
