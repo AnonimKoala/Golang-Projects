014c80795553fe9873d832074f462cb52bfd1677
Robert Kubasiak <robert2006goetel@gmail.com>
2024-11-18 16:50:25 +0100

    Implement ABAC policy evaluation and resource types
    - Add Role and Action constants for user roles and actions.
    - Define Subject struct to represent users with roles and blocked users.
    - Create Resource interface with methods for ID, author ID, and creation time.
    - Implement Comment and Todo structs that satisfy the Resource interface.
    - Develop ABACPolicy struct with Evaluate method to handle access control logic.
    - Add helper functions to check role presence and blocked status.

---
+++ abac-permissions/policy/policy-evaluator.go
@@ -0,0 +1,69 @@
+package policy
+
+type PolicyEvaluator interface {
+	Evaluate(subject Subject, action Action, resource Resource) bool
+}
+
+type ABACPolicy struct{}
+
+func (p ABACPolicy) Evaluate(subject Subject, action Action, resource Resource) bool {
+	// Admin has full access
+	if contains(subject.Roles, Admin) {
+		return true
+	}
+
+	// Moderator permissions
+	if contains(subject.Roles, Moderator) {
+		switch action {
+		case View, Update, Create:
+			return true
+		case Delete:
+			// todo test if the struct are the same, is allow to data assertion
+			if todo, ok := resource.(Todo); ok {
+				return todo.IsDone
+			}
+
+			if _, ok := resource.(Comment); ok {
+				return true
+			}
+
+			return false
+		}
+	}
+
+	// User permissions
+	if contains(subject.Roles, User) {
+		if isBlocked(subject, resource) {
+			return false
+		}
+
+		switch action {
+		case View, Create:
+			return true
+		case Update, Delete:
+			return resource.GetAuthorID() == subject.ID
+		}
+	}
+
+	return false
+}
+
+// contains checks if a given role is present in the list of Roles.
+func contains(roles []Role, role Role) bool {
+	for _, r := range roles {
+		if r == role {
+			return true
+		}
+	}
+	return false
+}
+
+// isBlocked checks if the subject is blocked by the author of the resource.
+func isBlocked(subject Subject, resource Resource) bool {
+	for _, blockedByID := range subject.BlockedBy {
+		if blockedByID == resource.GetAuthorID() {
+			return true
+		}
+	}
+	return false
+}
